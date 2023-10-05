b32e156f20c90a935a2029cd2bdd7d0f6798a115
이은정 <leeeunjeong@ieunjeong-ui-MacBookAir.local>
2023-10-05 19:08:54 +0900

    url이 슬래시일때 index.html 이 열려라

--- appcopy.js
+++ appcopy.js
@@ -1,5 +1,6 @@
 const http = require("http");
-const readFile = require("fs");
+const fs = require("fs");
+const { response } = require("express");
 //* http = node.js에서 사용하는 모듈이고 http 모듈은 server개체와 request(요청), response(응답) 객체를 사용한다.
 //*node.js 에 내장되어있는 함수  http 와 fs를 가져온다,
 
@@ -10,21 +11,29 @@ const server = http.createServer(function (request, response) {
   console.log(response);
 
   //*요청하는 urL이 </>라면 밑의 값을 나타내라
-  if (request.url === "/") {
-    let doc =
-      "<html> <head> <body> <a href='http://localhost:1234/sub'>main</a> </body> </head></html>";
+  response.writeHead(200, { "contant-Type": "text/html" }); //(상태코드, 헤더 정보)
 
-    response.writeHead(200, { "contant-Type": "text/html" }); //(상태코드, 헤더 정보)
-    //*contant-type은 html을 쓰되 나타내는건 text로 나타내는것
-    //*write = 본문(body)에 보여지는 부분을 쓰는 메서드이다.
-    response.end(doc);
-    //*응답을 종료하는 메서드
+  if (request.url === "/") {
+    fs.readFile("./index.html", function (err, data) {
+      if (err) {
+        console.err("파일을 읽지 못했습니다.");
+      } else {
+        response.end(data);
+      }
+    });
   }
-  if (request.url === "/sub") {
-    let doc =
-      "<html> <head> <body> <a href='http://localhost:1234'>sub</a> </body> </head></html>";
-    response.writeHead(200, { "contant-Type": "text/html" });
-    response.end(doc);
-  }
+  //*contant-type은 html을 쓰되 나타내는건 text로 나타내는것
+  //*write = 본문(body)에 보여지는 부분을 쓰는 메서드이다.
+  //*응답을 종료하는 메서드
+  // if (request.url === "/sub") {
+  //   response.writeHead(200, { "contant-Type": "text/html" });
+  //   fs.readFile("./index1.html", function (err, data) {
+  //     if (err) {
+  //       console.err("파일을 찾지 못했습니다.");
+  //     } else {
+  //       response.end(data);
+  //     }
+  //   });
+  // }
 });
 server.listen(1234);
