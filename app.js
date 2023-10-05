1616dc812932ee9d6d284a4d76ab04c256538c5a
이은정 <leeeunjeong@ieunjeong-ui-MacBookAir.local>
2023-10-05 15:42:53 +0900

    main page and sub page  ex

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+const { log } = require("console");
 const http = require("http");
 
 const server = http.createServer(function (request, response) {
-  //   console.log(request);
-  console.log(typeof response);
+  console.log(request.url);
+
+  if (request.url === "/") {
+    console.log("메인입니다.");
+  }
+  if (request.url === "/sub") {
+    console.log("서브입니다.");
+  }
+  // console.log(response);
   // 요청이 타입오브 클릭 이벤트 요청도 이벤트
   //   접속이라는 브라우저 형태로 바뀐것 뿐
-  response.writeHead(200, { "contant-Type": "text/html" });
+  // response.writeHead(200, { "contant-Type": "text/html" });
 
-  let doc = "<html> <head> <body> <h1> hello </h1></body> </head></html>";
-  response.end(doc);
+  //   let doc = "<html> <head> <body> <h1> hello </h1></body> </head></html>";
+  //   response.end(doc);
 });
 
 server.listen(1234);
